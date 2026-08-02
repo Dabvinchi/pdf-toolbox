a2f77cc4a790ee3af3e9320c98d95bba940e4d5e
Ann <dabyrick@gmail.com>
2026-08-02 10:13:09 +0800

    Added merge button

--- pdf-toolbox/src/App.jsx
+++ pdf-toolbox/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import UploadBox from "./components/UploadBox";
+import MergeButton from "./components/MergeButton";
 
 function App() {
   return (
     <div className="app">
       <Header />
       <UploadBox />
+      <MergeButton />
     </div>
   );
 }
---
+++ pdf-toolbox/src/components/MergeButton.jsx
@@ -0,0 +1,9 @@
+function MergeButton() {
+  return (
+    <button className="merge-button">
+      Merge PDFs
+    </button>
+  );
+}
+
+export default MergeButton;
